d589c59417928a3d221150923949634d6aefae97
heyDevhere <mdev3888@gmail.com>
2024-07-14 16:50:08 +0530

    changes

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -47,7 +47,7 @@ const EmblaCarousel = ({ fansLikeDetail }: CarouselProps) => {
             key={item._id}
             className="carousel_box"
             onClick={() =>
-              router.push(`/podcasts/${item.podcast[0]?.podcastId}`)
+              router.push(`/podcast/${item.podcast[0]?.podcastId}`)
             }
           >
             <Image
